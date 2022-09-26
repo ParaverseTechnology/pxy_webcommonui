f360b8c19900edfa0fcf6495bc3a9687ad3f5edc
fcx <fcx@pingxingyun.com>
2022-09-26 22:33:44 +0800

    dist

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,7 +1,13 @@
 import 'core-js/stable/symbol';
 import JoystickComponent from './components/Joystick.svelte';
+import Log from './utils/log';
+import Unit from './utils/unit';
+import Capabilities from './utils/capabilities';
 declare const _default: {
     JoystickComponent: typeof JoystickComponent;
     RepeateButtonComponent: typeof JoystickComponent;
+    Log: typeof Log;
+    Unit: typeof Unit;
+    Capabilities: typeof Capabilities;
 };
 export default _default;
