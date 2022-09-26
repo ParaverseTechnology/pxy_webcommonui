4ab13a2bac73350bcb82547f80eb95fecb00592c
fcx <fcx@pingxingyun.com>
2022-09-26 22:32:07 +0800

    add dist

---
+++ dist/types/index.d.ts
@@ -0,0 +1,7 @@
+import 'core-js/stable/symbol';
+import JoystickComponent from './components/Joystick.svelte';
+declare const _default: {
+    JoystickComponent: typeof JoystickComponent;
+    RepeateButtonComponent: typeof JoystickComponent;
+};
+export default _default;
